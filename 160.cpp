f2b876e3161769976b213c922e3bd0618a47b062
JohnBryan2822 <johnbryan2822@gmail.com>
2023-11-16 13:16:58 +0100

    changing letters in text - DSA

---
+++ 160.cpp
@@ -0,0 +1,11 @@
+// You have been given a text of no more than 500 words.
+// Your task is to convert lowercase letters to uppercase and uppercase letters in this text.
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    double x, y;
+    cin>>x>>y;
+    cout<<max(x, y)<<" "<<min(x, y);
+}
